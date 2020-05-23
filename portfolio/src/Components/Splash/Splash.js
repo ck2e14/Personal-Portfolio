5f07bf2b384783e97f1963548e48b6c76cff224a
ck2e14 <chriswkennedy@icloud.com>
2020-05-23 22:36:03 +0100

    minor viewport value tweaks in splash page, and imported other logo candidate for comaprison on development server.

--- portfolio/src/Components/Splash/Splash.js
+++ portfolio/src/Components/Splash/Splash.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import './Splash.css';
-import logo from '../../Assets/logo2.jpg'
+import logo2 from '../../Assets/logo2.jpg';
+import logo1 from '../../Assets/logo1.png';
 
 class Splash extends Component {
    render() {
       return (
          <div>
             <div className="background">
-               <img src={logo} alt="logo-main" className="logo-main"/>
+               <img src={logo2} alt="logo-main" className="logo-main"/>
             </div>
             <div className="splash-blurb">
                <span>Full-Stack<br></br> Web Developer</span>
                   <div className="divider-line"></div>
                   <div className="divider-line-2"></div>
                   <div className="blurb-statement">
-                     Passionate about designing and creating powerful, beautiful web apps to solve real-life problems  
+                     Passionate about designing and creating powerful, beautiful web apps to solve real-life problems
                   </div>
             </div>
          </div>
